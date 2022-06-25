49672a9e7348eb190ab1f2c26587f9c220e94cee
timur <Triglav761@example.com>
2022-06-25 17:36:51 +0500

    final

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Веб-приложение должно менять цветовые карты изображения r, g, b в соответствии
 # с заданным пользователем порядком, выдавать графики распределения цветов
 # исходной картинки и графики среднего значения цвета по вертикали и горизонтали.
+import cv2
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from werkzeug.utils import secure_filename
@@ -25,16 +26,18 @@ class ColorForm(FlaskForm):
                                               ("Dark2", "Dark2")],
                                      validators=[DataRequired()])
 
+#меняет цветовую карту изображения
+def color_pics(path, choosing_colormaps):
+    image = Image.open(path)
+    np_image = np.array(image)
+    plt.imshow(np_image[:, :, 1], cmap=choosing_colormaps)
+    plt.colorbar(orientation='vertical')
+    plt.title('Color map:', text=choosing_colormaps)
+    plt.savefig('./static/images/new_image.jpg')
+    plt.close()
 
-# выдавать графики распределения цветов исходной картинки и графики среднего значения цвета по вертикали и горизонтали.
-# Доделать выдачу средних значений.!!
-def color_map(path, filename):  # график
-    # очищаем папку static от файлов, загруженных в прошлой сессии
-    files = os.listdir("./static/images/")
-    if len(files) > 1:
-        for file_path in files:
-            if file_path != 'style.css':
-                os.remove('./static/images/' + file_path)
+
+def color_map(path):
     #  Создаем форму. В случае успешной валидации, переходим на страницу с результатом
     image = Image.open(path)
     # трансформируем изображение в numpy массив
@@ -48,48 +51,71 @@ def color_map(path, filename):  # график
     # создаем график по получившимся данным и сохраняем его в виде изображения
     fig = plt.figure(figsize=(4, 4))
     viewer = fig.add_subplot(1, 1, 1)
-    viewer.plot(rgb[0], color='r')
-    viewer.plot(rgb[1], color='g')
-    viewer.plot(rgb[2], color='b')
+    plt.plot(rgb[0], color='red', linestyle='solid', linewidth=1,
+             marker='o', markerfacecolor='blue', markersize=1)
+    plt.plot(rgb[1], color='green', linestyle='solid', linewidth=1,
+             marker='o', markerfacecolor='blue', markersize=1)
+    plt.plot(rgb[2], color='blue', linestyle='solid', linewidth=1,
+             marker='o', markerfacecolor='blue', markersize=1)
+    # setting x and y axis range
+    plt.ylim()
+    plt.xlim()
+
+    # именуем сторону x
+    plt.xlabel('x - axis')
+    # именуем сторону y
+    plt.ylabel('y - axis')
+
+    # giving a title to my graph
+    plt.title('График распределения цветов!')
     fig.savefig(f'./static/images/color_map.jpg')
+    plt.close()
     return 0
 
+###################
+# Load image as BGR
+def average_color_image(path):
+    image_bgr = cv2.imread(path, cv2.IMREAD_COLOR)
+    # Calculate the mean of each channel
+    rgb = cv2.mean(image_bgr)
 
-def color_map_2():  # график
-    image = Image.open('./static/images/new_image.jpg')
-    # трансформируем изображение в numpy массив
-    np_image = np.array(image)
-    # транспонируем, чтобы получить доступ к значениям RGB
-    image_transposed = np_image.transpose()
-    # создаем массивы и записываем в них количество значений для каждой величины R, G и B
-    rgb = [[]] * 3
-    for i in range(3):
-        rgb[i], bin = np.histogram(image_transposed[i], bins=256)
-    # создаем график по получившимся данным и сохраняем его в виде изображения
-    fig = plt.figure(figsize=(4, 4))
-    viewer = fig.add_subplot(1, 1, 1)
-    viewer.plot(rgb[0], color='r')
-    viewer.plot(rgb[1], color='g')
-    viewer.plot(rgb[2], color='b')
-    fig.savefig(f'./static/images/color_map_2.jpg')
-    return 0
+    # Swap blue and red values (making it RGB, not BGR)
+    observation = np.array([(rgb[0], rgb[1], rgb[2])])
+    # Show mean channel values
+    observation
+    # Show image
+    plt.imshow(observation), plt.axis("off")
+    plt.savefig(f'./static/images/average_color_image.jpg')
+    plt.show()
 
+##############
+# Load image as BGR
+def average_color_graph(path):
+    image_bgr = cv2.imread(path, cv2.IMREAD_COLOR)
+    # Calculate the mean of each channel
+    rgb = cv2.mean(image_bgr)
 
-collage_path = './static/images/collage.jpg'
+    # Swap blue and red values (making it RGB, not BGR)
+    np.array([(rgb[0], rgb[1], rgb[2])])
+    # Show mean channel values
+    print(rgb[1])
+    plt.plot(rgb[0], color='red', linestyle='dotted', linewidth=1,
+             marker='o', markerfacecolor='blue', markersize=8)
+    plt.plot(rgb[1], color='green', linestyle='dotted', linewidth=1,
+             marker='o', markerfacecolor='blue', markersize=8)
+    plt.plot(rgb[2], color='blue', linestyle='dotted', linewidth=1,
+             marker='o', markerfacecolor='blue', markersize=8)
+    # setting x and y axis range
+    plt.ylim()
+    plt.xlim()
 
-
-#############
-def color_pics(path, choosing_colormaps):
-    image = Image.open(path)
-    np_image = np.array(image)
-
-    plt.imshow(np_image[:, :, 1], cmap=choosing_colormaps)
-    plt.colorbar(orientation='horizontal')
-    plt.savefig('./static/images/new_image.jpg')
-
-    ############
-
-
+    plt.xlabel('horizontal')
+    plt.ylabel('vertical')
+    plt.title('График среднего значения цвета')
+    plt.savefig(f'./static/images/average_color_graph.jpg')
+    plt.show()
+    plt.close()
+##############
 # главная страница приложения
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -107,21 +133,19 @@ def index():
 
     if form.validate_on_submit():  # возвращает True, когда форма была отправлена и данные были приняты всеми валидаторами полей
 
-        filename1 = os.path.join('./static/images', secure_filename(form.img1.data.filename))
-        form.img1.data.save(filename1)
-        color_map(filename1, 'color_map')
-
-        color_pics(filename1, choosing_colormaps)
-        color_map_2()
-
-        return redirect(url_for("result", image1=filename1))
+        filename = os.path.join('./static/images', secure_filename(form.img1.data.filename))
+        form.img1.data.save(filename)
+        color_map(filename)
+        average_color_graph(filename)
+        average_color_image(filename)
+        color_pics(filename, choosing_colormaps)
+        return redirect(url_for("result", image1=filename))
 
     return render_template("index.html", form=form)
 
 
 @app.route("/result", methods=["GET"])
 def result():
-    # получаем названия файла и нужную форму коллажа из параметров функции redirect
+    # получаем названия файла и нужную форму из параметров функции redirect
     image1_path = request.args.get('image1')
-
     return render_template("result.html", image1=image1_path)
--- test.py
+++ test.py
@@ -1,111 +1,49 @@
-from PIL import Image
-import numpy as np
-#https://www.youtube.com/watch?v=3oXZTy7hrAQ&t=22s
-#https://gist.github.com/olooney/1246268
-#https://stackoverflow.com/questions/2270874/image-color-detection-using-python
+# Веб-приложение должно менять цветовые карты изображения r, g, b в соответствии
+# с заданным пользователем порядком, выдавать графики распределения цветов
+# исходной картинки и графики среднего значения цвета по вертикали и горизонтали.
+import cv2
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from werkzeug.utils import secure_filename
+from wtforms import FileField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileRequired, FileAllowed
+import os
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+#  Создаем форму. В случае успешной валидации, переходим на страницу с результатом
+image = Image.open('./static/images/1.jpg')
+# трансформируем изображение в numpy массив
+np_image = np.array(image)
+# транспонируем, чтобы получить доступ к значениям RGB
+image_transposed = np_image.transpose()
+rgb = [[]] * 3
+for i in range(3):
+    rgb[i], bin = np.histogram(image_transposed[i], bins=256)
+# x axis values
+#x = [1, 2, 3, 4, 5, 6]
+## corresponding y axis values
+#y = [2, 4, 1, 5, 2, 6]
 
-#pic = plt.imread('./static/1.jpg')
-#plt.imshow(pic)
-#print(pic)
-##get image pixels
-#print(pic.shape)
-#
-## display the image in a matplotlib window
-#
-#fig = plt.figure()
-#ax = fig.subplots()
-#f = ax.imshow(pic[:,:,1], alpha=0.8, cmap='gray', vmin=0, vmax=500, aspect='equal')
-#fig.colorbar(f)
-## crop the image
-##ax.set_xlim(0,1920)
-##ax.set_ylim(0,1920)
-#plt.savefig('./static/new_image.jpg')
-#plt.show()
-#ax.imshow(pic)
-#plt.show()
-#
+# plotting the points
+plt.plot(rgb[0], color='red', linestyle='solid', linewidth=1,
+         marker='o', markerfacecolor='blue', markersize=1)
+plt.plot(rgb[1], color='green', linestyle='solid', linewidth=1,
+         marker='o', markerfacecolor='blue', markersize=1)
+plt.plot(rgb[2], color='blue', linestyle='solid', linewidth=1,
+         marker='o', markerfacecolor='blue', markersize=1)
+# setting x and y axis range
+plt.ylim()
+plt.xlim()
 
-# for i in range(0, 200,1):
-#    if i < 50:
-#        val_list = [2] * 200
-#    elif i < 100:
-#        val_list = [5] * 200
-#    elif i < 150:
-#        val_list = [10] * 200
-#    vals.append(val_list)
-#
-# plt.pcolormesh (vals, cmap=plt.get_cmap('jet', 11), alpha=0.5)
-# plt.axis('off')
-# plt.colorbar()
-#
-# plt.show()
+# naming the x axis
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
 
-# np.random.seed(123)
-# data = np.random.rand(5, 7)
-# plt.pcolormesh(data, cmap='plasma', edgecolors="k", shading='flat')
-# plt.show()
+# giving a title to my graph
+plt.title('Some cool customizations!')
 
-## Преобразование данных изображения в массив для сохранения
-## img = np.asarray(Image.open('1.jpg').convert('RGB'))
-# x = Image.open('1.jpg').convert('RGB')
-# print(x.format)
-# print(x.mode)
-# print(x.size)
-##x.show()
-# x[56:112,:] = np.random.rand(3)[np.newaxis,np.newaxis,:]
-# x.show()
-# with Image.open('1.jpg') as im:
-#  im_data = np.array(im) # Преобразовать данные изображения в массив
-#  im_l = im.convert('L') # Серое изображение
-#  im_l_data = np.array(im_l) # Преобразование изображения в оттенках серого в массив
-#  im_l.save('11.jpg')
-#  open('11.jpg')
-# print("im_data.shape",im_data.shape) # Вывести форму массива цветных изображений, output (1920, 1080, 3)
-# print("im_l_data.shape", im_l_data.shape) # Вывести форму массива изображений в градациях серого, output (1920, 1080)
-##сохранять данные
-# np.savez('a_array.npz', a_dat = im_data, a_l_dat = im_l_data) # Сохраняем данные изображения в сжатый файл
-## Прочитать сохраненные данные
-# data = np.load('a_array.npz') # Загрузить данные, доступ к возвращаемым данным осуществляется через имя ключа и устанавливается как имя сохраненного массива
-# print(data.keys()) #Return ['a_dat', 'a_l_dat']
-## Оценка того, совпадают ли считанные и сохраненные данные
-# print(np.all(data[data.keys()[0]] == im_data), np.all(data[data.keys()[1]] == im_l_data)) #Output True True
-#
-#def color_pics(path):
-#    image = plt.imread(path)
-#    # трансформируем изображение в numpy массив
-#    np_image = np.array(image)
-#    # транспонируем, чтобы получить доступ к значениям RGB
-#    image_transposed = np_image.transpose()
-#    image_transposed.permute(1, 2, 0)
-#    print(image_transposed.shape) # вывод 3 300 400 /// требуется 300 400 3
-#    #Функция matplotlib 'imshow' получает 3-канальные изображения в виде (h, w, 3)
-#
-#    plt.imshow(image_transposed)
-#    f = plt.imshow(image_transposed[:, :, 1], cmap='plasma')
-#    plt.colorbar(f)
-#    plt.savefig('./new_image.jpg')
-
-image_open = Image.open('./static/images/Image.jpg')
-np_image = np.array(image_open)
-
-plt.imshow(np_image[:, :, 1], cmap='Blues')
-plt.savefig('./static/images/new_image.jpg')
-#image.open(path)
-#np_image = np.array(image)
-#plt.imshow(np_image[:,:,1], cmap=shape.lower())
-#И все никаких условий не надо даже
-#
-#
-#Попробуй так
-#
-#if shape.lower() == 'viridis':
-#if shape in ('Viridis', 'viridis'):
-#
-#А то твоя конструкция всегда истинна т.к. фактически ты проверяешь только shape == "foo",
-# а потом идёт всегда истинное or "bar"
-#
-#
-#
-#В if elif программа находит первое же условие и до условий после уже не доходит,
-# а когда 2 разных if то проверять будет каждый. даже если перед этим 1 уже удовлетворил условие
+# function to show the plot
+plt.show()
